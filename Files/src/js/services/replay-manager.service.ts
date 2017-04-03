11605be2628138570463a99ce3c3ad0b28dbbedb
sebastientromp <sebastien.tromp@gmail.com>
2017-04-03 19:53:12 +0200

    Change zip file extension and fix encoding issue in converters

--- Files/src/js/services/replay-manager.service.ts
+++ Files/src/js/services/replay-manager.service.ts
@@ -17,7 +17,7 @@ export class ReplayManager {
 
 	public saveLocally(game: Game) {
 		let directory = this.plugin.get().LOCALAPPDATA + '/ZeroToHeroes/Overwolf/Replays/';
-		let fileName = game.player.name + '(' + game.player.class + ')_vs_' + game.opponent.name + '(' + game.opponent.class + ')_' + new Date().getTime() + '.zip';
+		let fileName = game.player.name + '(' + game.player.class + ')_vs_' + game.opponent.name + '(' + game.opponent.class + ')_' + new Date().getTime() + '.hszip';
 		console.log('saving locally', directory + fileName);
 		this.plugin.get().writeLocalAppDataZipFile(directory + fileName, "replay.xml", game.replay, (status, message) => {
 			console.log('local zip file saved', status, message);
