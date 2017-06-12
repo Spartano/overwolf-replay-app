102407e92a5e98c1ad510b4c99e64b672cfa3000
sebastientromp <sebastien.tromp@gmail.com>
2017-06-12 20:52:04 +0200

    Empty state fixes

--- Files/src/js/components/empty-shelf.component.ts
+++ Files/src/js/components/empty-shelf.component.ts
@@ -9,7 +9,8 @@ import { Component } from '@angular/core';
 			<img src="static/images/clockwork.png" alt="">
 
 			<h1>No Replay</h1>
-			<p>We found no match to replay in this session.<br>If you did play a match, please <a href="#">contact us</a></p>
+			<p>We found no match to replay in this session.<br>
+		If you did play a match, check our <a href="http://support.overwolf.com/article-categories/zerotoheroes/" target="_blank">support page</a></p> 
 		</div>
 	`,
 })
